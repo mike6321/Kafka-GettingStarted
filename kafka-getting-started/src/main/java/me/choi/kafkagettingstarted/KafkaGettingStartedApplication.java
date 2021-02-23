f782c7a3f749c07dc6d47b2165cc0560d5703a0f
junwoo <33277588+mike6321@users.noreply.github.com>
2021-02-24 01:43:28 +0900

    Producer 생성

---
+++ kafka-getting-started/src/main/java/me/choi/kafkagettingstarted/KafkaGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package me.choi.kafkagettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaGettingStartedApplication.class, args);
+    }
+
+}
---
+++ kafka-getting-started/src/main/java/me/choi/kafkagettingstarted/SimpleProducer.java
@@ -0,0 +1,47 @@
+package me.choi.kafkagettingstarted;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+/**
+ * Project : kafka-getting-started
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 1:05 오전
+ */
+@Component
+public class SimpleProducer {
+//    @Value("${junwoo.topic.name}")
+    private static String TOPIC_NAME = "junwoo";
+//    @Value("${junwoo.bootstrapAddress}")
+    private static String BOOTSTRAP_SERVERS = "18.224.200.228:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        System.out.println(BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        for (int index = 0; index < 10; index++) {
+            String data = "This is record " + index;
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, data);
+            try {
+                producer.send(record);
+                System.out.println("Send to " + TOPIC_NAME + " | data : " + data);
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
