79ae16d90158e43ddd8a07f9df8dddcf6d122ff6
junwoo <33277588+mike6321@users.noreply.github.com>
2021-02-26 00:05:17 +0900

    (Kafka) multithreading

---
+++ kafka-getting-started/src/main/java/me/choi/kafkagettingstarted/multi/ConsumerWithMultiThread.java
@@ -0,0 +1,49 @@
+package me.choi.kafkagettingstarted.multi;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Project : kafka-getting-started
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 12:01 오전
+ */
+public class ConsumerWithMultiThread {
+    private static String TOPIC_NAME = "test";
+    private static String GROUP_ID = "testgroup";
+    private static String BOOTSTRAP_SERVERS = "18.224.200.228:9092";
+    private static int CONSUMER_COUNT = 3;
+    private static List<ConsumerWorker> workerThreads = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < CONSUMER_COUNT; i++) {
+            ConsumerWorker worker = new ConsumerWorker(configs, TOPIC_NAME, i);
+            workerThreads.add(worker);
+            executorService.execute(worker);
+        }
+    }
+
+    static class ShutdownThread extends Thread {
+        public void run() {
+            workerThreads.forEach(ConsumerWorker::shutdown);
+            System.out.println("Bye");
+        }
+    }
+}
